83e1472e9853689ecf54f4d8ab61e80bdaa7885c
C-luigi <luigi.chiappa@le-campus-numerique.fr>
2023-10-10 15:22:40 +0200

    script.sh

---
+++ .github/workflows/ServLaucnhScript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+## script made for launching application on server after release from github action on master branch
+# it need to be launched on server permanently (with nohup for example) with jar located saved in
+# jarDirectory variable
+jarDirectory='/var/www/Projetpsicine';
+function restartJar()
+{
+  PID_jar_process=$(ps aux | grep .jar | grep -v 'grep' | awk '{print $2}');
+  kill $PID_jar_process;
+  java -jar "$jarDirectory/*.jar" &
+  echo "";
+}
+while true :
+do
+  watch --chgexit -n 120 "ls -l --full-time $jarDirectory |grep .jar | sha256sum" && restartJar;
+done
